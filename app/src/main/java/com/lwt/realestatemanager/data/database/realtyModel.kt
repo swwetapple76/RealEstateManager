e72cfef7035225ceff965cc1d11daee6be54f799
LAI WAH TSE <laiwah76@gmail.com>
2022-11-23 14:08:34 +0100

    clean up update

--- app/src/main/java/com/lwt/realestatemanager/data/database/realtyModel.kt
+++
@@ -1,9 +0,0 @@
-package com.lwt.realestatemanager.data.database
-
-data class RealtyModel(
-    // on below line we are creating variables for name and job
-    var realtyName: String,
-    var realtyType: String,
-    var realtyAddress: String,
-    var realtyPriceInDollars: String
-)
