78291e4ca3bf1d2ff63191f0382b17373180b1fb
LAI WAH TSE <laiwah76@gmail.com>
2022-11-25 12:12:31 +0100

    clean up res

--- app/src/main/java/com/lwt/realestatemanager/theme/Color.kt
+++
@@ -1,8 +0,0 @@
-package com.lwt.realestatemanager.theme
-
-import androidx.compose.ui.graphics.Color
-
-val primaryLight = Color(0xFF9575CD)
-val primaryDark = Color(0xFF673AB7)
-val primaryVariant = Color(0xFF512DA8)
-val accentuationColor = Color(0xFFEC407A)
